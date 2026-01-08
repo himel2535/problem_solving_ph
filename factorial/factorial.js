5dfd613c0434ad07ff5e9a60cba319cf74a77cc2
himel2535 <monwarhossanhimel@gmail.com>
2026-01-08 18:34:26 +0600

    factorial

---
+++ factorial/factorial.js
@@ -0,0 +1,12 @@
+
+const factorial=(n)=>{
+    if(n===0 || n===1){
+        return 1;
+    }
+    let result=1;
+    for(let i=2;i<=n;i++){
+        result=result*i;
+    }
+    return result;
+}
+console.log(factorial(5));
